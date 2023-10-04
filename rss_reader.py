c4b544dc3cf8d2f3e511dfaf16de22ff6ca984aa
arturasVis <artvis44@gmail.com>
2023-10-04 21:20:18 +0100

    Finished

---
+++ rss_reader.py
@@ -0,0 +1,19 @@
+import feedparser
+import requests
+
+def fetch_feed(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return feedparser.parse(response.content)
+
+def display_feed(feed):
+    for entry in feed.entries:
+        print(f'Title: {entry.title}')
+        print(f'Link: {entry.link}')
+        print(f'Description: {entry.description}')
+        print('-' * 50)  # Print a separator line
+
+if __name__ == "__main__":
+    url = input()
+    feed = fetch_feed(url)
+    display_feed(feed)
